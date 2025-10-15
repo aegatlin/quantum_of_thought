ca039a685c50a9d88d35afebbc09ff2ecdf2aaf2
Austin Gatlin <austin@gatlin.io>
2025-10-15 13:54:47 +1100

    aic

---
+++ web_client/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-background flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold tracking-tight">
+          Quantum of Thought
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
