adc3020106c0a007ae9bc8e08e4a009a94f05a34
Austin Gatlin <austin@gatlin.io>
2025-10-20 12:56:20 +1100

    fix ui/css issues with shadcn and tailwindcss

--- client_web/vite.config.ts
+++ client_web/vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import { defaultExclude } from "vitest/config";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
+import { defaultExclude } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [react(), tailwindcss(), wasm(), topLevelAwait()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
